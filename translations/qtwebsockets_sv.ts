68e54380ea501a16a932ae39f1bc309fe8d046f7
Daniel Nylander <po@danielnylander.se>
2024-12-18 15:39:45 +0100

    Adding Swedish qtwebsockets

---
+++ translations/qtwebsockets_sv.ts
@@ -0,0 +1,336 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>QQmlWebSocket</name>
+    <message>
+        <location filename="../../qtwebsockets/src/imports/qmlwebsockets/qqmlwebsocket.cpp" line="+134"/>
+        <location line="+10"/>
+        <source>Messages can only be sent when the socket is open.</source>
+        <translation>Meddelanden kan endast skickas när uttaget är öppet.</translation>
+    </message>
+    <message>
+        <location line="+57"/>
+        <source>QQmlWebSocket is not ready.</source>
+        <translation>QQmlWebSocket är inte redo.</translation>
+    </message>
+</context>
+<context>
+    <name>QQmlWebSocketServer</name>
+    <message>
+        <location filename="../../qtwebsockets/src/imports/qmlwebsockets/qqmlwebsocketserver.cpp" line="+151"/>
+        <source>QQmlWebSocketServer is not ready.</source>
+        <translation>QQmlWebSocketServer är inte redo.</translation>
+    </message>
+</context>
+<context>
+    <name>QWebSocket</name>
+    <message>
+        <location filename="../../qtwebsockets/src/websockets/qwebsocket_p.cpp" line="+200"/>
+        <source>Connection closed</source>
+        <translation>Anslutningen är stängd</translation>
+    </message>
+    <message>
+        <location line="+236"/>
+        <source>Invalid URL.</source>
+        <translation>Ogiltig URL.</translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Invalid resource name.</source>
+        <translation>Ogiltigt resursnamn.</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>SSL Sockets are not supported on this platform.</source>
+        <translation>SSL-uttag stöds inte på denna plattform.</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <location line="+25"/>
+        <source>Out of memory.</source>
+        <translation>Slut på minne.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <location filename="../../qtwebsockets/src/websockets/qwebsocket_wasm_p.cpp" line="+170"/>
+        <source>Unsupported WebSocket scheme: %1</source>
+        <translation>WebSocket-schemat stöds inte: %1</translation>
+    </message>
+    <message>
+        <location line="+359"/>
+        <source>Error writing bytes to socket: %1.</source>
+        <translation>Fel vid skrivning av bytes till uttag: %1.</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Bytes written %1 != %2.</source>
+        <translation>Bytes skrivna %1 != %2.</translation>
+    </message>
+    <message>
+        <location line="+71"/>
+        <source>QWebSocketPrivate::processHandshake: Unsupported WWW-Authenticate challenge encountered.</source>
+        <extracomment>&apos;WWW-Authenticate&apos; is the HTTP header.</extracomment>
+        <translation>QWebSocketPrivate::processHandshake: WWW-Authenticate-challenge stöds inte.</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>QWebSocketPrivate::processHandshake: Unsupported WWW-Authenticate challenges encountered.</source>
+        <translation>QWebSocketPrivate::processHandshake: WWW-Authenticate-challenges stöds inte.</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Header is too large</source>
+        <translation>Rubriken är för stor</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Read handshake request header failed</source>
+        <translation>Rubrik för läshandskakningsbegäran misslyckades</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Read handshake request status failed</source>
+        <translation>Status för läshandskakningsbegäran misslyckades</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Parsing handshake request header failed</source>
+        <translation>Tolkning av begäransrubrik för handskakning misslyckades</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>WebSocket server has chosen protocol %1 which has not been requested</source>
+        <translation>WebSocket-servern har valt protokollet %1 som inte har begärts</translation>
+    </message>
+    <message>
+        <location line="+24"/>
+        <source>Accept-Key received from server %1 does not match the client key %2.</source>
+        <translation>Accept-Key som togs emot från servern %1 matchar inte klientnyckeln %2.</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Invalid parameter encountered during protocol upgrade: %1</source>
+        <translation>Ogiltig parameter påträffades under protokolluppgradering :%1</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Handshake: Server requests a version that we don&apos;t support: %1.</source>
+        <translation>Handskakning: Servern begär en version som vi inte har stöd för: %1.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <location line="+4"/>
+        <source>QWebSocketPrivate::processHandshake: Unknown error condition encountered. Aborting connection.</source>
+        <translation>QWebSocketPrivate::processHandshake: Okänt felvillkor påträffades. Avbryter anslutningen.</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>QWebSocket::processHandshake: Host requires authentication</source>
+        <translation>QWebSocket::processHandshake: Värden kräver autentisering</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>QWebSocketPrivate::processHandshake: Unhandled http status code: %1 (%2).</source>
+        <translation>QWebSocketPrivate::processHandshake: Ohanterad http-statuskod: %1 (%2).</translation>
+    </message>
+    <message>
+        <location line="+227"/>
+        <source>The resource name contains newlines. Possible attack detected.</source>
+        <translation>Resursnamnet innehåller nyrader. Potentiell attack upptäcktes.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>The hostname contains newlines. Possible attack detected.</source>
+        <translation>Värdnamnet innehåller nyrader. Potentiell attack upptäcktes.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>The origin contains newlines. Possible attack detected.</source>
+        <translation>Ursprunget innehåller nyrader. Potentiell attack upptäcktes.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>The extensions attribute contains newlines. Possible attack detected.</source>
+        <translation>Attributet tillägg innehåller nyrader. Potentiell attack upptäcktes.</translation>
+    </message>
+    <message>
+        <location filename="../../qtwebsockets/src/websockets/qwebsocket_wasm_p.cpp" line="-17"/>
+        <source>Connection refused</source>
+        <translation>Anslutningen nekades</translation>
+    </message>
+</context>
+<context>
+    <name>QWebSocketDataProcessor</name>
+    <message>
+        <location filename="../../qtwebsockets/src/websockets/qwebsocketdataprocessor.cpp" line="+157"/>
+        <source>Received Continuation frame, while there is nothing to continue.</source>
+        <translation>Tog emot Continuation-ram, men det finns ingenting att fortsätta.</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>All data frames after the initial data frame must have opcode 0 (continuation).</source>
+        <translation>Alla dataramar efter den initiala dataramen måste ha opcode 0 (continuation).</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Received message is too big.</source>
+        <translation>Tog emot meddelande som är för stort.</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <location line="+93"/>
+        <source>Invalid UTF-8 code encountered.</source>
+        <translation>Ogiltig UTF-8-kod togs emot.</translation>
+    </message>
+    <message>
+        <location line="-14"/>
+        <source>Payload of close frame is too small.</source>
+        <translation>Nyttolast för stängningsram är för liten.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Invalid close code %1 detected.</source>
+        <translation>Ogiltig stängningskod %1 upptäcktes.</translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>Invalid opcode detected: %1</source>
+        <translation>Ogiltig opcode upptäcktes: %1</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Timeout when reading data from socket.</source>
+        <translation>Tidsgräns överstegs vid läsning av data från uttag.</translation>
+    </message>
+</context>
+<context>
+    <name>QWebSocketFrame</name>
+    <message>
+        <location filename="../../qtwebsockets/src/websockets/qwebsocketframe.cpp" line="+67"/>
+        <source>Waiting for more data from socket.</source>
+        <translation>Väntar på mer data från uttaget.</translation>
+    </message>
+    <message>
+        <location line="+163"/>
+        <source>Error occurred while reading header from the network: %1</source>
+        <translation>Fel inträffade vid läsning av rubrik från nätverket: %1</translation>
+    </message>
+    <message>
+        <location line="+46"/>
+        <source>Error occurred while reading from the network: %1</source>
+        <translation>Fel inträffade vid läsning från nätverket: %1</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Lengths smaller than 126 must be expressed as one byte.</source>
+        <translation>Längder mindre än 126 måste uttryckas som en byte.</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Something went wrong during reading from the network.</source>
+        <translation>Någonting gick fel vid läsning från nätverket.</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Highest bit of payload length is not 0.</source>
+        <translation>Högsta biten för nyttolastlängden är inte 0.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Lengths smaller than 65536 (2^16) must be expressed as 2 bytes.</source>
+        <translation>Längder mindre än  65536 (2^16) måste uttryckas som 2 bytes.</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Error while reading from the network: %1.</source>
+        <translation>Fel vid läsning från nätverket: %1.</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Maximum framesize exceeded.</source>
+        <translation>Maximal ramstorlek överstegs.</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Some serious error occurred while reading from the network.</source>
+        <translation>Några allvarliga fel inträffade vid läsning från nätverket.</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Rsv field is non-zero</source>
+        <translation>Rsv-fältet är icke-noll</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Used reserved opcode</source>
+        <translation>Använt reserverad opcode</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Control frame is larger than 125 bytes</source>
+        <translation>Kontrollram är större än 125 bytes</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Control frames cannot be fragmented</source>
+        <translation>Kontrollramar kan inte fragmenteras</translation>
+    </message>
+</context>
+<context>
+    <name>QWebSocketHandshakeResponse</name>
+    <message>
+        <location filename="../../qtwebsockets/src/websockets/qwebsockethandshakeresponse.cpp" line="+122"/>
+        <source>Access forbidden.</source>
+        <translation>Åtkomst förbjuden.</translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>Unsupported version requested.</source>
+        <translation>Begärd version stöds inte.</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>One of the headers contains a newline. Possible attack detected.</source>
+        <translation>En av rubrikerna innehöll en nyrad. Potentiell attack upptäcktes.</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Bad handshake request received.</source>
+        <translation>Felaktig handskakningsbegäran togs emot.</translation>
+    </message>
+</context>
+<context>
+    <name>QWebSocketServer</name>
+    <message>
+        <location filename="../../qtwebsockets/src/websockets/qwebsocketserver_p.cpp" line="+126"/>
+        <source>Server closed.</source>
+        <translation>Servern är stängd.</translation>
+    </message>
+    <message>
+        <location line="+336"/>
+        <source>Header is too large.</source>
+        <translation>Rubriken är för stor.</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Too many pending connections.</source>
+        <translation>För många väntande anslutningar.</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Read handshake request header failed.</source>
+        <translation>Begäransrubrik för läshandskakning misslyckades.</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>Upgrade to WebSocket failed.</source>
+        <translation>Uppgradering till WebSocket misslyckades.</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Invalid response received.</source>
+        <translation>Ogiltigt svar togs emot.</translation>
+    </message>
+</context>
+</TS>
